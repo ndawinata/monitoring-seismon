135a343b0abd8db1c7b327cf633fc0e0efa01689
ndawinata <nanda.winata@gmail.com>
2022-02-14 12:59:52 +0800

    pertama

---
+++ app.py
@@ -0,0 +1,95 @@
+from flask import Flask, json, render_template, request, make_response, session, redirect, url_for, flash, abort, jsonify, send_file, Response
+from mysql.connector.utils import read_bytes
+from werkzeug.utils import secure_filename
+import os, sys
+from docxtpl import DocxTemplate, InlineImage
+from DateTime import DateTime
+import time
+import calendar
+from datetime import datetime
+from openpyxl import Workbook, load_workbook
+from openpyxl.styles import Alignment
+import mysql.connector
+from docx.shared import Mm
+from flask_cors import CORS
+import uuid
+import io
+import base64
+from PIL import Image
+import json
+import subprocess
+# sumber mongodb : https://www.w3schools.com/python/python_mongodb_getstarted.asp
+
+# import datetime
+
+app = Flask(__name__)
+CORS(app)
+app.secret_key = 'randomterserah'
+
+# upload gambar
+app.config['UPLOAD_FOLDER'] = 'static'
+
+# mydb = mysql.connector.connect(
+#     host="127.0.0.1",
+#     user="root",
+#     password="",
+#     database="nandapro_seismon"
+# )
+
+# biar tidak error saat get data yang kedua dst
+
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'json'}
+
+@app.route('/command', methods=['GET', 'POST'])
+def command():
+    if request.method == 'POST':
+        req = request.json
+        if req['command'] == 'restart':
+            result = subprocess.check_output("systemctl restart seismon.service", shell=True, text=True, encoding="utf8")
+            dat={
+                "output":result
+            }
+            
+            return dat
+        elif req['command'] == 'stop':
+            result = subprocess.check_output("systemctl stop seismon.service", shell=True, text=True, encoding="utf8")
+            dat={
+                "output":result
+            }
+            
+            return dat
+        elif req['command'] == 'start':
+            result = subprocess.check_output("systemctl start seismon.service", shell=True, text=True, encoding="utf8")
+            dat={
+                "output":result
+            }
+            
+            return dat
+        elif req['command'] == 'pull':
+            result = subprocess.check_output("git pull origin master; systemctl restart seismon.service;", shell=True, text=True, encoding="utf8")
+            dat={
+                "output":result
+            }
+            
+            return dat
+        elif req['command'] == 'status':
+            result = subprocess.check_output("systemctl status seismon.service", text=True, shell=True)
+            dat={
+                "output":result
+            }
+            
+            return dat
+
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+        'status': 404,
+        'message': 'Not Found ' + request.url
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+    return resp
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True, port=4000)
